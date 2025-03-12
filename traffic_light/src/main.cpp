3eea6830690d29992e7b86de5475f89eb4a979b9
Kim001104 <dongsky2000@naver.com>
2025-03-13 00:36:31 +0900

    task scheduler 사용 두개 led동시에 켜짐

--- traffic_light/src/main.cpp
+++ traffic_light/src/main.cpp
@@ -1,47 +1,56 @@
 #include <Arduino.h>
 #include <TaskScheduler.h>
 
-#define LED_PIN 13
+const int redLedPin = 13;
+const int yellowLedPin = 12;
+const int greenLedPin = 11;
 
-// TaskScheduler 인스턴스 생성
 Scheduler runner;
 
-// LED를 켜고 끄는 작업 정의
-void LedOn();
-void LedOff();
-void CheckSerialInput();
+void toggleRedLed();
+void toggleYellowLed();
+void toggleGreenLed();
 
-Task t1(2000, TASK_FOREVER, &LedOn, &runner, true);
-Task t2(0, TASK_ONCE, &LedOff, &runner, true);
-Task t3(100, TASK_FOREVER, &CheckSerialInput, &runner, true);
+Task tRedLed(1000, TASK_FOREVER, &toggleRedLed, &runner, true);
+Task tYellowLed(1000, TASK_FOREVER, &toggleYellowLed, &runner, false);
+Task tGreenLed(1000, TASK_FOREVER, &toggleGreenLed, &runner, false);
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT);
-  Serial.begin(9600);
+  pinMode(redLedPin, OUTPUT);
+  pinMode(yellowLedPin, OUTPUT);
+  pinMode(greenLedPin, OUTPUT);
 }
 
 void loop() {
   runner.execute();
 }
 
-void LedOn() {
-  Serial.println("[" + String(millis()) + "]LED ON");
-  digitalWrite(LED_PIN, HIGH);
-  t2.restartDelayed(200);  // t2 작업을 2초 뒤에 다시 시작
+void toggleRedLed() {
+  static bool state = false;
+  digitalWrite(redLedPin, state ? HIGH : LOW);
+  state = !state;
+  if (!state) {
+    tRedLed.disable();
+    tYellowLed.enable();
+  }
 }
 
-void LedOff() {
-  Serial.println("[" + String(millis()) + "]LED OFF");
-  digitalWrite(LED_PIN, LOW);
+void toggleYellowLed() {
+  static bool state = false;
+  digitalWrite(yellowLedPin, state ? HIGH : LOW);
+  state = !state;
+  if (!state) {
+    tYellowLed.disable();
+    tGreenLed.enable();
+  }
 }
 
-void CheckSerialInput() {
-  if (Serial.available() > 0) {
-    String input = Serial.readStringUntil('\n');
-    int newPeriod = input.toInt();
-    if (newPeriod > 0) {
-      t1.setInterval(newPeriod);
-      Serial.println("New period: " + String(newPeriod));
-    }
+void toggleGreenLed() {
+  static bool state = false;
+  digitalWrite(greenLedPin, state ? HIGH : LOW);
+  state = !state;
+  if (!state) {
+    tGreenLed.disable();
+    tRedLed.enable();
   }
 }
